ce23e0028602210f24deaa922244c1f0137dc1ad
Engr. Peter Goteh <edwardpeter547@gmail.com>
2022-05-16 19:27:44 +0100

    created additional models for the model class, also updated the User model with password field

--- blog/models.py
+++ blog/models.py
@@ -2,16 +2,17 @@ from sqlalchemy import Integer, String, DateTime, Float, Column
 from datetime import datetime
 from .database import Base, engine, session_local
 
-class User():
+class User(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True, index=True)
     fullname = Column(String)
     email = Column(String)
     mobile = Column(String)
+    password = Column(String, nullable=False)
     datecreated = Column(DateTime, default = datetime.utcnow, nullable=False)
     
     
-class Blog():
+class Blog(Base):
     __tablename__ = "blog"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
@@ -19,9 +20,10 @@ class Blog():
     datecreated = Column(DateTime, default=datetime.utcnow, nullable=False)
     
     
-class Category():
+class Category(Base):
     __tablename__ = "category"
     id = Column(Integer, primary_key=True, index=True)
     catname = Column(String)
+    description = Column(String, nullable=True)
     datecreated = Column(DateTime, default=datetime.utcnow, nullable=False)
     
