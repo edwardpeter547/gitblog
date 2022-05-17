125ced75da6e2df4af3568f6d789eb244d506c3d
Engr. Peter Goteh <edwardpeter547@gmail.com>
2022-05-17 09:34:11 +0100

    created the route for the users subsystem. this provides definition of all routes to the users subsystem.

---
+++ blog/routes/user.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, status, Depends
+from sqlalchemy.orm import Session
+from .. import database, schemas
+from .. repo.user import UserRepo
+
+
+router = APIRouter(tags=["Users"], prefix="/user")
+
+get_db = database.get_db
+
+
+# Todo: list all users from db:
+@router.get('/list', status_code = status.HTTP_200_OK)
+def list_users(db: Session = Depends(get_db)):
+    return UserRepo.list(db=db)
+
+
+# Todo: show user with user id {id} from db:
+@router.get('/view/{id}', status_code = status.HTTP_200_OK)
+def get_user(id: int, db: Session = Depends(get_db)):
+    return UserRepo.get(id=id, db=db)
+
+
+# Todo: remove user with user id {id} from db:
+@router.delete('/remove/{id}', status_code = status.HTTP_200_OK)
+def del_user(id: int, db: Session = Depends(get_db)):
+    return UserRepo.delete(id=id, db=db)
+
+
+# Todo: create user from User (schemas)
+@router.post('/create')
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    return UserRepo.create(request=request, db=db)
+
+
+# Todo: update user with user id {id} from db
+@router.put('/update/{id}', status_code = status.HTTP_200_OK)
+def udpate_user(id: int, request: schemas.User, db: Session = Depends(get_db)):
+    return UserRepo.update(id=id, request=request, db=db)
