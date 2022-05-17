8752ad6036d983f544246c98883990b2838f7399
Engr. Peter Goteh <edwardpeter547@gmail.com>
2022-05-17 09:32:10 +0100

    created the route for the post subsystem. this provides definition of all routes to the post subsystem.

---
+++ blog/routes/blog.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, status, Depends, HTTPException
+from sqlalchemy.orm import Session
+from .. import models, database, schemas
+from .. repo.blog import Blogrepo
+
+
+router = APIRouter(tags=["Post"], prefix="/post")
+
+get_db = database.get_db
+
+# Todo: list all post from db:
+@router.get('/list', status_code = status.HTTP_200_OK)
+def bloglist(db: Session = Depends(get_db)):  # type: ignore
+    return Blogrepo.list(db=db)
+
+
+# Todo: get post with post id {id} from db:
+@router.get('/view/{id}', status_code=status.HTTP_200_OK)
+def get_blog(id: int, db: Session = Depends(get_db)):
+    return Blogrepo.get(id=id, db=db)
+
+
+# Todo: remove post with post id {id} from db:
+@router.delete('/remove/{id}', status_code = status.HTTP_200_OK)
+def del_blog(id: int, db: Session = Depends(get_db)):
+    return Blogrepo.delete(id=id, db=db)
+
+
+# Todo: create post using Blog from (schemas)
+@router.post('/create', status_code=status.HTTP_201_CREATED)
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
+    return Blogrepo.create(request=request, db=db)
+
+
+# Todo: update post with post id {id} from db:
+@router.put('/update/{id}', status_code = status.HTTP_200_OK)
+def udpate_blog(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    return Blogrepo.udpate(id=id, request=request, db=db)
