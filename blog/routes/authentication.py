5f92bfdb2591b7aa0d632767921b3688e83a867e
Engr. Peter Goteh <edwardpeter547@gmail.com>
2022-05-18 20:02:05 +0100

    refactored the login route to use OAuth2PasswordRequestForm

--- blog/routes/authentication.py
+++ blog/routes/authentication.py
@@ -3,6 +3,7 @@ from .. import schemas, database, models
 from sqlalchemy.orm import Session
 from .. hashing import Hash
 from .. repo.authentication import AuthRepo
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 router = APIRouter(tags=["Authentication"], prefix="/auth")
@@ -11,5 +12,5 @@ get_db = database.get_db
 
 
 @router.post('/login')
-def auth_login(request: schemas.Auth, db: Session = Depends(get_db)):
-    return AuthRepo.login(request=request, db=db)
+def auth_login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    return AuthRepo.login(request=request, db=db) 
