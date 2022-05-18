402cc13cff29584e572e2d44f77ac0dd69f893fc
Engr. Peter Goteh <edwardpeter547@gmail.com>
2022-05-18 20:04:18 +0100

    refactored the AuthRepo class, login classmethod to create jwt token after password verification

--- blog/repo/authentication.py
+++ blog/repo/authentication.py
@@ -1,17 +1,21 @@
 from fastapi import HTTPException, status
-from .. import models, schemas
+from .. import models, schemas, jwtoken
 from sqlalchemy.orm import Session
 from .. hashing import Hash
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 class AuthRepo():
     
     
     @classmethod
-    def login(cls, request: schemas.Auth, db: Session):
-        user = db.query(models.User).filter(models.User.email == request.email).first()
+    def login(cls, request: OAuth2PasswordRequestForm, db: Session):
+        user = db.query(models.User).filter(models.User.email == request.username).first()
         if not user:
-            raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail = f"email {request.email} is incorrect!")
+            raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail = f"invalid login credentials for user with email: {request.email}")
         if not Hash.verify(plain_password=request.password, hashed_password= user.password):
-            raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail = f"email {request.email} is incorrect!")
-        return {"message": f"user with email {request.email} has been logged in successfully."}
+            raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail = f"invalid login credentials for user with email: {request.email}")
+        
+        credentials = {"sub": user.email}
+        access_token = jwtoken.create_access_token(user_data=credentials, expire_minutes=None)
+        return {"access_token": access_token, "token_type": "bearer"}
